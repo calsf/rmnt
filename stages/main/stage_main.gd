e07b0ebdd066e05a6bf83e51bf2e4a361e1c5281
calsf <lefrogjones@gmail.com>
2023-09-15 16:51:23 -0400

    moving files and setting up boss stage select interact

--- stages/main/stage_main.gd
+++ stages/main/stage_main.gd
@@ -3,6 +3,8 @@ extends Node2D
 onready var rmnt_select = $CanvasLayer/RmntSelect
 onready var stage_select_mob_ui = $CanvasLayer/StageSelectMob
 onready var stage_select_mob_interactable = $World/StageSelectMob
+onready var stage_select_boss_ui = $CanvasLayer/StageSelectBoss
+onready var stage_select_boss_interactable = $World/StageSelectBoss
 
 onready var players = get_tree().get_nodes_in_group("players")
 
@@ -11,8 +13,9 @@ var is_overlapping_player_hurtbox := false
 
 
 func _input(event):
-	# Toggle stage select off if activated
+	# Toggle UIs off if activated
 	# May be triggered outside the interactable area so we need to be able to handle separately
+	# Mob stage select
 	if event.is_action_pressed("interact") and stage_select_mob_ui.activated:
 		for player in players:
 			if player.visible:
@@ -23,7 +26,19 @@ func _input(event):
 		stage_select_mob_interactable.set_can_interact(true)
 		return
 	
-	# If in interactable area for stage select
+	# Boss stage select
+	if event.is_action_pressed("interact") and stage_select_boss_ui.activated:
+		for player in players:
+			if player.visible:
+				player.pause_player(false)
+		
+		stage_select_boss_ui.deactivate()
+		rmnt_select.activate()
+		stage_select_boss_interactable.set_can_interact(true)
+		return
+	
+	# Toggle UIs on
+	# Mob stage select
 	if stage_select_mob_interactable.is_interactable:
 		if event.is_action_pressed("interact"):
 			if not stage_select_mob_ui.activated:
@@ -34,3 +49,15 @@ func _input(event):
 				stage_select_mob_ui.activate()
 				rmnt_select.deactivate()
 				stage_select_mob_interactable.set_can_interact(false)
+	
+	# Boss stage select
+	if stage_select_boss_interactable.is_interactable:
+		if event.is_action_pressed("interact"):
+			if not stage_select_boss_ui.activated:
+				for player in players:
+					if player.visible:
+						player.pause_player(true)
+				
+				stage_select_boss_ui.activate()
+				rmnt_select.deactivate()
+				stage_select_boss_interactable.set_can_interact(false)
