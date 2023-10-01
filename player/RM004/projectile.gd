ec1f54126ea7b72d0954138d19f1406922588c1c
calsf <lefrogjones@gmail.com>
2023-09-30 20:34:53 -0400

    add air attacks

--- player/RM004/projectile.gd
+++ player/RM004/projectile.gd
@@ -1,8 +1,14 @@
 class_name Projectile
 extends Node2D
 
-export var _speed := 0
+export var _speed_x := 0
+export var _speed_y := 0
+export var min_y := 0
+export var has_y_movement := false
 export var destroy_on_enemy_hit := true
+
+onready var projectile_sprite = $Projectile/Sprite
+onready var projectile_hitbox = $Projectile/PlayerHitbox
 
 var dir : Vector2
 var is_facing_left := false
@@ -13,7 +19,17 @@ func _process(delta):
 
 
 func _move(delta):
-	position += (dir * _speed) * delta
+	position += (dir * _speed_x) * delta
+	
+	# Move projectile child sprite down, destroys projectile once reaches min_y
+	if has_y_movement:
+		if projectile_sprite.position.y >= min_y:
+			projectile_sprite.position.y = min_y
+			projectile_hitbox.position.y = min_y
+			destroy()
+		else:
+			projectile_sprite.position += (Vector2.DOWN * _speed_y) * delta
+			projectile_hitbox.position = projectile_sprite.position
 
 
 func set_dir(new_dir : Vector2) -> void:
@@ -30,6 +46,11 @@ func turn(facing_x) -> void:
 		self.scale.y = 1
 		self.rotation_degrees = 0
 		is_facing_left = false
+
+
+func set_offset_y(offset : Vector2) -> void:
+	projectile_sprite.position += offset
+	projectile_hitbox.position += offset
 
 
 func destroy() -> void:
