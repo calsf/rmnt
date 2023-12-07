36a86a9c8fc141bde8ef68155e974c0f4765b84c
calsf <lefrogjones@gmail.com>
2023-12-06 21:11:32 -0500

    updating en009

--- stages/stage_mobs001.gd
+++ stages/stage_mobs001.gd
@@ -5,7 +5,7 @@ func _ready():
 	# Init stage props
 	enemy_listing = [
 		[Enemies.EN000],
-		[Enemies.EN005],
+		[Enemies.EN009],
 		[Enemies.EN008],
 #		[Enemies.EN006],
 #		[Enemies.EN001, Enemies.EN002, Enemies.EN003],
