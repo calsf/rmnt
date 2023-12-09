56d3e9387adc4cd2c4d4ae3a59ea8cb6eede977b
calsf <lefrogjones@gmail.com>
2023-12-08 19:24:09 -0500

    add rm001 special

--- player/player_state_attack_special.gd
+++ player/player_state_attack_special.gd
@@ -3,7 +3,10 @@ extends PlayerState
 export var armored := false
 export var projectile_path := ""
 export var spawn_offset : Vector2
+export var is_falling_attack := false
 
+var moving_dir := 0
+var has_landed := false
 var Projectile
 
 
@@ -39,6 +42,15 @@ func enter(data_state := {}) -> void:
 	player.is_aerial_stun = false
 	
 	player.disable_hurtbox()
+	
+	# Set x movement direction based only initial input
+	if Input.is_action_pressed("move_left"):
+		moving_dir = -1
+	elif Input.is_action_pressed("move_right"):
+		moving_dir = 1
+	else:
+		moving_dir = 0
+	has_landed = false
 
 
 func exit(data_state := {}) -> void:
@@ -47,6 +59,8 @@ func exit(data_state := {}) -> void:
 	player.armored = false
 	
 	player.enable_hurtbox()
+	moving_dir = 0
+	has_landed = false
 
 
 func handle_input(event: InputEvent) -> void:
@@ -56,3 +70,17 @@ func handle_input(event: InputEvent) -> void:
 func state_process(delta: float) -> void:
 	if not player.anim.is_playing():
 		state_machine.transition_to("Idle")
+
+
+func state_physics_process(delta: float) -> void:
+	if not has_landed and player.get_player_child().is_on_floor():
+		has_landed = true
+	
+	if is_falling_attack:
+		# Maintain x velocity from initial input only while in air
+		if not has_landed:
+			player.velocity.x = player.props.speed_x * moving_dir
+			player.velocity = player.move_and_slide(player.velocity)
+	
+		player.child_velocity.y += player.GRAVITY * delta
+		player.child_velocity = player.get_player_child().move_and_slide(player.child_velocity, Vector2.UP)
