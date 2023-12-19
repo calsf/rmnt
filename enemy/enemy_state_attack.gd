3cdad6bdfffad15d70af770c4f74f52095281c05
calsf <lefrogjones@gmail.com>
2023-12-18 21:14:21 -0500

    add en010 attack a anim

--- enemy/enemy_state_attack.gd
+++ enemy/enemy_state_attack.gd
@@ -46,9 +46,7 @@ func exit(data_state := {}) -> void:
 
 
 func state_physics_process(delta: float) -> void:	
-	if not enemy.enemy_child.is_on_floor():
-		state_machine.transition_to("HitstunAir")
-	elif enemy.is_aerial_stun:
+	if enemy.is_aerial_stun:
 		state_machine.transition_to("HitstunAir")
 	elif enemy.knockback != Vector2.ZERO:
 		state_machine.transition_to("Hitstun")
