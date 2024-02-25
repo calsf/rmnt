4e7e672958bb974045529aa3e7c3ac051c65ef39
calsf <lefrogjones@gmail.com>
2024-02-25 14:35:14 -0500

    bs003 setup

--- stages/stage_normal.gd
+++ stages/stage_normal.gd
@@ -4,6 +4,7 @@ class_name StageNormal
 const Enemies = {
 	BS001 = "res://enemy/BS001/BS001.tscn",
 	BS002 = "res://enemy/BS002/BS002.tscn",
+	BS003 = "res://enemy/BS003/BS003.tscn",
 	HEALTH = "res://enemy/Potions/PotionHealth.tscn",
 	METER = "res://enemy/Potions/PotionMeter.tscn",
 	EN000 = "res://enemy/EN000/EN000.tscn",
